00d73a61c494ffe307ec899fa798e0ff6567d68f
gauravano <sachdeva.gaurav1997@gmail.com>
2019-02-18 03:01:16 +0530

    bullets added

--- scripts/index.js
+++ scripts/index.js
@@ -9,7 +9,7 @@ document.body.appendChild(canvasElement);
 
 console.log(canvas);
 var FPS = 30;
-
+var playerBullets = [];
 
 var player = {
     color: "#00A",
@@ -19,24 +19,90 @@ var player = {
     height: 32,
 
     draw: function(){
-        console.log("draw called!");
         canvas.fillStyle = this.color;
         canvas.fillRect(this.x, this.y, this.width, this.height);
     }
 };
 
+function Bullet(I){
+    I.active = true;
+    I.xVelocity = 0;
+    I.yVelocity = -I.speed;
+    I.width = 3;
+    I.height = 3;
+    I.color = "#000";
+    
+    I.inBounds = function(){
+        return (I.x >= 0 && I.x <= CANVAS_WIDTH) && (I.y >= 0 && I.y <= CANVAS_HEIGHT);
+    };
+
+    I.draw = function(){
+        canvas.fillStyle = this.color;
+        canvas.fillRect(this.x, this.y, this.width, this.height);
+    };
+
+    I.update = function(){
+        I.x += I.xVelocity;
+        I.y += I.yVelocity;
+
+        I.active = I.active && I.inBounds();
+    };
+
+    return I;
+}
+
 function draw(){
     canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     player.draw();
-}
 
+    playerBullets.forEach(function(bullet){
+        bullet.draw();
+    });
+}
 
 function update(){
+    if(keydown.left){
+        player.x -= 5;
+    }
+    
+    if(keydown.right){
+        player.x += 5;
+    }
 
+    if(keydown.space){
+        player.shoot();
+    }
+
+    player.x = player.x.clamp(0, CANVAS_WIDTH - player.width);
+
+    playerBullets.forEach(function(bullet){
+        bullet.update();
+    });
+
+    playerBullets = playerBullets.filter(function(bullet){
+        return bullet.active;
+    });
 }
 
-// setInterval(function(){
-//     update();
-    
-// }, 1000/FPS);
-draw();
+player.shoot = function(){
+    var bulletPosition = this.midpoint();
+
+    playerBullets.push(Bullet({
+        speed: 5,
+        x: bulletPosition.x,
+        y: bulletPosition.y
+    }));
+};
+
+player.midpoint = function(){
+    return {
+        x: this.x + this.width/2,
+        y: this.y + this.height/2
+    }
+};
+
+setInterval(function(){
+    draw();
+    update();
+}, 1000/FPS);
+
