c56b8d8a9ae172a3724de8bfa7dfdb8ac72131de
gauravano <sachdeva.gaurav1997@gmail.com>
2019-02-18 03:36:14 +0530

    enemies added

--- scripts/index.js
+++ scripts/index.js
@@ -10,6 +10,7 @@ document.body.appendChild(canvasElement);
 console.log(canvas);
 var FPS = 30;
 var playerBullets = [];
+var enemies = [];
 
 var player = {
     color: "#00A",
@@ -51,12 +52,50 @@ function Bullet(I){
     return I;
 }
 
+function Enemy(I){
+    I = I || {};
+
+    I.active = true;
+    I.age = Math.floor(Math.random() * 128);
+    I.color = "#A2B";
+    I.x = CANVAS_WIDTH/4 + Math.random() * CANVAS_WIDTH/2;
+    I.y = 0;
+    I.xVelocity = 0;
+    I.yVelocity = 2;
+    I.width = 32;
+    I.height = 32;
+
+    I.inBounds = function(){
+        return (I.x >= 0 && I.x <= CANVAS_WIDTH) && (I.y >=0 && I.y <= CANVAS_HEIGHT);
+    };
+
+    I.draw = function(){
+        canvas.fillStyle = this.color;
+        canvas.fillRect(this.x, this.y, this.width, this.height);
+    };
+
+    I.update = function(){
+        I.x += I.xVelocity;
+        I.y += I.yVelocity;
+
+        I.xVelocity = 3 * Math.sin(I.age * Math.PI/64);
+        I.age++;
+        I.active = I.active && I.inBounds();
+    };
+
+    return I;
+}
+
 function draw(){
     canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     player.draw();
 
     playerBullets.forEach(function(bullet){
         bullet.draw();
+    });
+
+    enemies.forEach(function(enemy){
+        enemy.draw();
     });
 }
 
@@ -82,6 +121,18 @@ function update(){
     playerBullets = playerBullets.filter(function(bullet){
         return bullet.active;
     });
+
+    enemies.forEach(function(enemy){
+        enemy.update();
+    });
+
+    enemies = enemies.filter(function(enemy){
+        return enemy.active;
+    });
+
+    if(Math.random() < 0.1){
+        enemies.push(Enemy());
+    }
 }
 
 player.shoot = function(){
